59a0b5047dcefb42488c3febe6674249c03d7f00
Fillipe <incarnialol2@gmail.com>
2025-08-20 11:48:50 -0300

    PalindromeNumber

---
+++ src/main/java/org/example/PalindromeNumber.java
@@ -0,0 +1,39 @@
+package org.example;
+
+/*  -------------------------------------------------------------------------------------------------------------------------
+    PROBLEM 09
+    Problem Description:
+    Given an integer x, return true if x is a palindrome, and false otherwise.
+    -------------------------------------------------------------------------------------------------------------------------
+ */
+
+public class PalindromeNumber {
+
+    //Runtime: 7ms 13.73%
+    public boolean SolutionUsingStrings(int x) {
+        if (x < 0) { return false; }
+        String xString = String.valueOf(x);
+        for(int i = 0; i < Math.floor((double) xString.length() / 2); i++) {
+            if (xString.charAt(i) != xString.charAt(xString.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Runtime 4ms 100%
+    public boolean SolutionWithRemainder(int x) {
+        if (x < 0) { return false; }
+
+        int reverse = 0;
+        int copy = x;
+
+        while(x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x = x / 10;
+        }
+
+        return reverse == copy;
+    }
+
+}
