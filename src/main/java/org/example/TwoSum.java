7e3b83edadeef19436877a2a396bb0271e930ff6
Fillipe <incarnialol2@gmail.com>
2025-08-20 11:29:37 -0300

    TwoSum

---
+++ src/main/java/org/example/TwoSum.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.HashMap;
+import java.util.Map;
+
+/*  -------------------------------------------------------------------------------------------------------------------------
+    Problem Description:
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+    -------------------------------------------------------------------------------------------------------------------------
+    Runtime: 2ms 98.92%
+ */
+
+public class TwoSum {
+
+    public int[] Solution(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+}
